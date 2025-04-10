c3a8aed8c83281fafac43882117097235b7f594f
Gustav Brodin <brodin.gustav@gmail.com>
2025-04-11 00:12:57 +0200

    Commit - Satt kontroll ifall användare redan finns vid registrering

--- src/main/java/com/example/App/controller/AuthController.java
+++ src/main/java/com/example/App/controller/AuthController.java
@@ -18,48 +18,63 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Collections;
 import java.util.Map;
 
-@RestController 
-@RequestMapping("/api/auth") 
+@RestController
+@RequestMapping("/api/auth")
 public class AuthController {
 
-    //Injecting Dependencies
-    @Autowired private UserRepository userRepository;
-    @Autowired private JWTUtil jwtUtil;
-    @Autowired private AuthenticationManager authManager;
-    @Autowired private PasswordEncoder passwordEncoder;
+    // Injecting Dependencies
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private JWTUtil jwtUtil;
+    @Autowired
+    private AuthenticationManager authManager;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
-    //POST för register
+    // POST för register
     @PostMapping("/register")
-    public RegisterDTO registerHandler(@RequestBody User user){
-        
+    public RegisterDTO registerHandler(@RequestBody User user) {
+
+        // Kontroll om användare redan finns
+        if (userRepository.existsByEmail(user.getEmail())) {
+            throw new RuntimeException("Användare finns redan.");
+        }
+
         // Encoding Password med Bcrypt
         String encodedPass = passwordEncoder.encode(user.getPassword());
 
         // Sätter det encoded password
         user.setPassword(encodedPass);
 
-        //Spara användare
+        //Sätter användarens namn om det inte är null
+        if (user.getName() == null || user.getName().isEmpty()) {
+            user.setName("Anonym"); //Fallback
+        }
+
+        // Spara användare
         user = userRepository.save(user);
 
         // Genererar JWT
         String token = jwtUtil.generateToken(user.getEmail());
 
-         // Returnera namn, email och token
+        // Returnera namn, email och token
         return new RegisterDTO(user.getName(), user.getEmail(), token);
 
-       /*  Svarar with JWT
-        return Collections.singletonMap("jwt-token", token);
-        */
-        }
+        /*
+         * Svarar with JWT
+         * return Collections.singletonMap("jwt-token", token);
+         */
+    }
 
     // Definierar funktion som hanterar POST rutt för att logga in en user
     @PostMapping("/login")
-    public Map<String, Object> loginHandler(@RequestBody LoginCredentials body){
+    public Map<String, Object> loginHandler(@RequestBody LoginCredentials body) {
         try {
-            //Skapar en Authentication Token som innehåller credentials för authenticating
+            // Skapar en Authentication Token som innehåller credentials för authenticating
             // Token används som input till authentication process
-            UsernamePasswordAuthenticationToken authInputToken =
-                    new UsernamePasswordAuthenticationToken(body.getEmail(), body.getPassword());
+            UsernamePasswordAuthenticationToken authInputToken = new UsernamePasswordAuthenticationToken(
+                    body.getEmail(), body.getPassword());
 
             // Authenticating the Login Credentials
             authManager.authenticate(authInputToken);
@@ -70,13 +85,11 @@ public class AuthController {
 
             // Svara med JWT
             return Collections.singletonMap("jwt-token", token);
-        }catch (AuthenticationException authExc){
-            // Auhentication Failed
+        } catch (AuthenticationException authExc) {
+
+            System.err.println("Error during authentication: " + authExc.getMessage());
             throw new RuntimeException("Invalid Login Credentials");
         }
     }
-    
-
 
 }
-
